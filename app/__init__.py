e125839e15375668e77ea781affeafc7e41ecd38
dmaish <mainadaniel81@gmail.com>
2018-03-25 11:17:07 +0300

    change flask object from flask to flask_API, write the API endpoints tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 # third parth imports
-from flask import Flask
+from flask import Flask, jsonify
+from flask_api import FlaskAPI
 
 # local imports
 from config import app_config
@@ -7,8 +8,8 @@ from config import app_config
 
 # the following method accepts environment variable as its variable
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
+    app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    # app.config.from_pyfile('config.py')
 
     return app
---
+++ tests.py
@@ -0,0 +1,65 @@
+import unittest
+import json
+from app import create_app
+
+
+class BookListApi(unittest.TestCase):
+    """class representing the test case"""
+
+    def setUp(self):
+        """test variables and initialisation"""
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context.push()
+        self.book = {
+            'title': 'The Da Vinci Code',
+            'author': 'Dan Brown',
+            'category': 'Mystery Thriller'
+        }
+
+
+    def test_new_book_creation(self):
+        """Test if API can create a new book(POST request)"""
+        post_res = self.client().post('/books/', data=self.book)
+        self.assertEqual(post_res.status_code, 201)
+        self.assertIn('The Da Vinci Code', str(post_res.data))
+
+    def test_get_all_books(self):
+        """Test if API can get all books"""
+        post_res = self.client().post('/books/', data=self.book)
+        self.assertEqual(post_res.status_code, 201)
+        get_res = self.client().get('/books/')
+        self.assertEqual(get_res.status_code, 200)
+        self.assertIn('The Da Vinci Code', str(get_res.data))
+
+    def test_get_book_by_id(self):
+        """Test if book can be retrieved by id"""
+        post_res = self.client().post('/books/', data=self.book)
+        self.assertEqual(post_res.status_code, 201)
+        json_response = json.loads(post_res.data.decode('utf-8').replace("'", "\""))
+        get_res = self.client().get('/books/{}'.format(json_response['id']))
+        self.assertEqual(get_res.status_code, 200)
+        self.assertIn('The Da Vinci Code', str(get_res.data))
+
+    def test_can_edit_book(self):
+        """Test if API can edit an existing book"""
+        post_res = self.client().post('/books/', data=self.book)
+        self.assertEqual(post_res.status_code, 201)
+        put_res = self.client().put('/books/1',data={'title': 'inferno'})
+        self.assertEqual(put_res.status_code, 200)
+        results = self.client().get('/books/1')
+        self.assertIn('inferno', str(results.data))
+
+    def test_book_deletion(self):
+        "Test if API can delete an existing book.(DELETE request)"
+        post_res = self.client().post('/books/', data=self.book)
+        self.assertEqual(post_res.status_code, 201)
+        del_res = self.client().delete('/books/1')
+        self.assertEqual(del_res.status_code, 404)
+
+    def tearDown(self):
+        self.app.app_context.pop()
+
+
+if __name__ == '__main__':
+    unittest.main()
