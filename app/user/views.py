61888e6416ce10d27f9a136f8d12c5af375b6a41
dmaish <mainadaniel81@gmail.com>
2018-05-21 16:33:10 +0300

    [Feature #157693587]borrowing history with date and time of transactions

--- app/user/views.py
+++ app/user/views.py
@@ -4,13 +4,15 @@ from flask import jsonify
 # local imports
 from . import user
 from ..models import *
-import logging
+import datetime
 
 
 @user.route("/api/users/books/<int:id>", methods=["POST"])
 @jwt_required
 def borrow_book(id):
     """Retrieve a specific book and allow user to borrow"""
+    now = datetime.datetime.now()
+    time = now.strftime("%B %d, %Y %H:%M")
     # get book being borrowed
     book = Book.query.filter_by(id=id).first()
 
@@ -20,7 +22,7 @@ def borrow_book(id):
 
     # borrowing record
     if not book.borrowed_flag:
-        record = Borrow_Record(user=borrower, book=book)
+        record = Borrow_Record(user=borrower, book=book, time_borrowed=time)
         book.borrowed_flag = True
         db.session.add(record)
         db.session.commit()
@@ -28,6 +30,7 @@ def borrow_book(id):
         response = {
             "user borrowed": record.user_borrowed,
             "book borrowed": record.book_id,
+            "time borrowed": record.time_borrowed,
             "message": "book borrowing success"
         }
         return jsonify(response), 200
@@ -42,6 +45,10 @@ def borrow_book(id):
 @user.route("/api/users/books/<int:id>", methods=["PUT"])
 @jwt_required
 def return_book(id):
+    """method to allow user to return a specific borrowed book"""
+    now = datetime.datetime.now()
+    time = now.strftime("%B %d, %Y %H:%M")
+
     user_email = get_jwt_identity()
     user = User.get_user_by_email(user_email)
     book = Book.query.filter_by(id=id).first()
@@ -49,10 +56,12 @@ def return_book(id):
                                                      user_borrowed=user.id,
                                                      return_flag=False).first()
     borrowing_record.return_flag = True
+    borrowing_record.time_returned = time
     book.borrowed_flag = False
     db.session.commit()
     response = {
-        "message": "book successfully returned"
+        "message": "book successfully returned",
+        "time returned":borrowing_record.time_returned
     }
     return jsonify(response), 200
 
@@ -97,8 +106,8 @@ def get_user_borrowing_history():
         record = {"title": book.title,
                   "author": book.author,
                   "category": book.category,
-                  "date_borrowed": "-",
-                  "date_returned": "-",
+                  "time borrowed": each_record.time_borrowed,
+                  "time returned": each_record.time_returned,
                   "return_flag": each_record.return_flag}
 
         history.append(record)
